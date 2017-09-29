47353235ceffa2c2c1362c2cd7ea255e60d79df0
Artur Gussi de Oliveira <arturgussi@gmail.com>
2017-09-29 20:34:27 -0300

    first master

---
+++ index.js
@@ -0,0 +1,13 @@
+//index.js
+
+module.exports = function(tempF){
+	if(typeof tempF === 'string'){
+		tempF = tempF.trim().replace(',','.') 
+	}
+
+	let tempC = parseFloat(tempF)
+	tempC -= 32
+	tempC /= 1.8
+	//return parseInt((tempF * 100) / 100)
+	return tempC.toFixed(2)
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# f2c-arturgussi
+
+This package returns celsius temperture from fahrenheit value
+
+## Usage
+
+```javascript
+
+const f2c = require('f2c-arturgussi')
+const tempC = f2c(tempF)
+```
